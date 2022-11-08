dc21b188830518220dafa8a89ebca9b0f0aae9f1
ChimpOnCloud <sunfy20@mails.tsinghua.edu.cn>
2022-11-08 19:20:16 +0800

    synchronize

--- client/config/index.js
+++ client/config/index.js
@@ -14,6 +14,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
+    // host: '127.0.0.1',
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -1,39 +1,68 @@
 <template>
-  <div>
-    <el-container>
-      <el-header>Login Page</el-header>
-      <el-main>
-        username
-        <el-input v-model="input_username" placeholder="请输入内容"></el-input>
-        password
-        <el-input v-model="input_password" placeholder="请输入内容"></el-input>
-      </el-main>
-    </el-container>
+  <div class="login">
+    <div class="loginBox">
+      <h2 class="loginH2"><strong>Login your</strong> Fightdiabetes</h2>
+      <div class="loginCon">
+        <div class="titleDiv">
+          <h3>Sign up now</h3>
+          <p>Enter your username and password to log on:</p>
+          <!-- <p><i class="el-icon-user"></i></p>
+          <i class="el-icon-key"></i> -->
+        </div>
+        <el-form ref="loginForm" :rules="rules" :model="ruleForm">
+          <el-form-item prop="user">
+            <el-input placeholder="请输入账号" prefix-icon="el-icon-user" v-model="ruleForm.username"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input placeholder="请输入密码" prefix-icon="el-icon-lock" v-model="ruleForm.password" show-password>
+            </el-input>
+          </el-form-item>
+          <el-button type="primary" class="loginBtn" @click="send_data()">登录</el-button>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+// import SlideVerify from '@/components/SlideVerify'
+import axios from 'axios'
 export default {
   data () {
     return {
-      input_username: '',
-      input_password: ''
+      ruleForm: {
+        username: '',
+        password: ''
+      }
     }
   }
 }
+// send_data () {
+
+// }
+var data = {
+  username: this.username,
+  password: this.password
+}
+var config = {
+  method: 'post',
+  url: 'http://127.0.0.1/login',
+  headers: {
+    'User-Agent': 'Apifox/1.0.0 (https://www.apifox.cn)',
+    'Content-Type': 'application/json'
+  },
+  data: data
+}
+
+axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data))
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
 </script>
 
 <style>
-  .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: left;
-    line-height: 60px;
-  }
+
 </style>
