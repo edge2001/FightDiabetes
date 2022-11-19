0b619816d027c60fa301eb299b063af88f8a7ccb
ChimpOnCloud <sunfy20@mails.tsinghua.edu.cn>
2022-11-19 16:16:05 +0800

    login and register ok

--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <div class="loginBox">
-      <h1 class="loginH2" ><strong>Login your</strong> Fightdiabetes</h1>
+      <h1 class="loginH2"><strong>Login your</strong> Fightdiabetes</h1>
       <div class="loginCon">
         <div class="titleDiv">
           <!-- <button class="signup_button" @click="jump_to_Register">Sign up now</button> -->
@@ -49,200 +49,213 @@ export default {
       window.location.href = '/#/register'
     },
     send_data: function () {
+      var dataobj = {
+        username: this.ruleForm.username,
+        password: this.ruleForm.password
+      }
+      var config = {
+        method: 'post',
+        url: 'http://127.0.0.1:8000/login/',
+        headers: {
+          'User-Agent': 'Apifox/1.0.0 (https://www.apifox.cn)',
+          'Content-Type': 'application/json'
+        },
+        data: dataobj
+      }
       axios(config)
         .then(function (response) {
           console.log(JSON.stringify(response.data))
+          if (response.status === 200) {
+            window.location.href = '/#/MainPage'
+          }
         })
         .catch(function (error) {
           console.log(error)
+          if (error.response.status === 401) {
+            window.alert('密码错误')
+          } else if (error.response.status === 404) {
+            window.alert('用户不存在')
+          }
         })
     }
   }
-}
-var data = {
-  username: this.username,
-  password: this.password
-}
-var config = {
-  method: 'post',
-  url: 'http://127.0.0.1:8000/login/',
-  headers: {
-    'User-Agent': 'Apifox/1.0.0 (https://www.apifox.cn)',
-    'Content-Type': 'application/json'
-  },
-  data: data
 }
 </script>
 
 <style>
 html {
-      background: linear-gradient(to right,
-          rgba(186, 218, 179, 1) 30%,
-          rgba(241, 164, 60, 1) 30%,
-          rgba(241, 164, 60, 1) 70%,
-          rgba(213, 100, 42, 1) 70%);
-      height: 100%;
-    }
+  background: linear-gradient(to right,
+      rgba(186, 218, 179, 1) 30%,
+      rgba(241, 164, 60, 1) 30%,
+      rgba(241, 164, 60, 1) 70%,
+      rgba(213, 100, 42, 1) 70%);
+  height: 100%;
+}
 
-    /* $color: #f1a43c;
+/* $color: #f1a43c;
     $color2: #bdd9b2;
     $color3: #f1ebc9; */
+body {
+  padding: 2rem;
+  height: 100%;
+}
 
-    body {
-      padding: 2rem;
-      height: 100%;
-    }
+h1 {
+  font-size: 5vw;
+  top: 80%;
+  left: 0;
+  margin: 0;
+  padding: 0 2rem;
+  box-sizing: border-box;
+  transform: translate(0, -50%);
+  position: absolute;
+  color: #f1ebc9;
+  width: 100%;
+  text-align: center;
+  font-family: 'Lobster', cursive;
+}
 
-    h1 {
-      font-size: 5vw;
-      top: 80%;
-      left: 0;
-      margin: 0;
-      padding: 0 2rem;
-      box-sizing: border-box;
-      transform: translate(0, -50%);
-      position: absolute;
-      color: #f1ebc9;
-      width: 100%;
-      text-align: center;
-      font-family: 'Lobster', cursive;
-    }
+.signup_button {
+  position: relative;
+  right: 20px;
+  bottom: 20px;
+  top: 90%;
+  right: 50%;
+  border: none;
+  box-shadow: none;
+  width: 130px;
+  height: 40px;
+  line-height: 42px;
+  -webkit-perspective: 230px;
+  perspective: 230px;
+  left: 45%;
+}
 
-    .signup_button {
-      position: relative;
-      right: 20px;
-      bottom: 20px;
-      top:90%;
-      right: 50%;
-      border: none;
-      box-shadow: none;
-      width: 130px;
-      height: 40px;
-      line-height: 42px;
-      -webkit-perspective: 230px;
-      perspective: 230px;
-      left: 45%;
-    }
+.signup_button {
+  background: rgb(0, 172, 238);
+  background: linear-gradient(0deg, rgba(0, 172, 238, 1) 0%, rgba(2, 126, 251, 1) 100%);
+  display: block;
+  position: absolute;
+  width: 130px;
+  height: 40px;
+  box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, .5),
+    7px 7px 20px 0px rgba(0, 0, 0, .1),
+    4px 4px 5px 0px rgba(0, 0, 0, .1);
+  border-radius: 5px;
+  margin: 0;
+  text-align: center;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  -webkit-transition: all .3s;
+  transition: all .3s;
+}
 
-    .signup_button {
-      background: rgb(0, 172, 238);
-      background: linear-gradient(0deg, rgba(0, 172, 238, 1) 0%, rgba(2, 126, 251, 1) 100%);
-      display: block;
-      position: absolute;
-      width: 130px;
-      height: 40px;
-      box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, .5),
-        7px 7px 20px 0px rgba(0, 0, 0, .1),
-        4px 4px 5px 0px rgba(0, 0, 0, .1);
-      border-radius: 5px;
-      margin: 0;
-      text-align: center;
-      -webkit-box-sizing: border-box;
-      -moz-box-sizing: border-box;
-      box-sizing: border-box;
-      -webkit-transition: all .3s;
-      transition: all .3s;
-    }
-    .signup_button {
-      width: 130px;
-      height: 40px;
-      line-height: 42px;
-      padding: 0;
-      border: none;
-      background: rgb(255, 27, 0);
-      background: linear-gradient(0deg, rgba(255, 27, 0, 1) 0%, rgba(251, 75, 2, 1) 100%);
-    }
-    .signup_button:hover {
-      color: #f0094a;
-      background: transparent;
-      box-shadow: none;
-    }
+.signup_button {
+  width: 130px;
+  height: 40px;
+  line-height: 42px;
+  padding: 0;
+  border: none;
+  background: rgb(255, 27, 0);
+  background: linear-gradient(0deg, rgba(255, 27, 0, 1) 0%, rgba(251, 75, 2, 1) 100%);
+}
 
-    .signup_button:before,
-    .signup_button:after {
-      content: '';
-      position: absolute;
-      top: 0;
-      right: 0;
-      height: 2px;
-      width: 0;
-      background: #f0094a;
-      box-shadow:
-        -1px -1px 5px 0px #fff,
-        7px 7px 20px 0px #0003,
-        4px 4px 5px 0px #0002;
-      transition: 400ms ease all;
-    }
-    .signup_button:hover:after {
-      left: auto;
-      right: 0;
-      width: 100%;
-    }
+.signup_button:hover {
+  color: #f0094a;
+  background: transparent;
+  box-shadow: none;
+}
 
-    .signup_button:after {
-      right: inherit;
-      top: inherit;
-      left: 0;
-      bottom: 0;
-    }
+.signup_button:before,
+.signup_button:after {
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 2px;
+  width: 0;
+  background: #f0094a;
+  box-shadow:
+    -1px -1px 5px 0px #fff,
+    7px 7px 20px 0px #0003,
+    4px 4px 5px 0px #0002;
+  transition: 400ms ease all;
+}
 
-    .signup_button:hover:before,
-    .signup_button:hover:after {
-      width: 100%;
-      transition: 800ms ease all;
-    }
+.signup_button:hover:after {
+  left: auto;
+  right: 0;
+  width: 100%;
+}
 
-    .intro_text {
-      font-family: 'Lobster', cursive;
-      margin-right: 55px;
-    }
+.signup_button:after {
+  right: inherit;
+  top: inherit;
+  left: 0;
+  bottom: 0;
+}
 
-    p {
-      font-family: 'Lato', sans-serif;
-      font-weight: 300;
-      text-align: center;
-      font-size: 28px;
-      color: #676767;
-      top: 30%;
-    }
-    /* .btn-in {
+.signup_button:hover:before,
+.signup_button:hover:after {
+  width: 100%;
+  transition: 800ms ease all;
+}
+
+.intro_text {
+  font-family: 'Lobster', cursive;
+  margin-right: 55px;
+}
+
+p {
+  font-family: 'Lato', sans-serif;
+  font-weight: 300;
+  text-align: center;
+  font-size: 28px;
+  color: #676767;
+  top: 30%;
+}
+
+/* .btn-in {
       height: 400px;
       width: 600px;
       margin: 500px;
     } */
-    .intro_text {
-      text-align: right;
-    }
-    .btn-1 {
-      left: 45%;
-      margin-top: 50px;
-      align-self: center;
-      text-align: center;
-      width: 130px;
-      height: 40px;
-      color: #fff;
-      border-radius: 5px;
-      padding: 10px 25px;
-      font-family: 'Lato', sans-serif;
-      font-weight: 500;
-      /* background: transparent; */
-      cursor: pointer;
-      transition: all 0.3s ease;
-      position: relative;
-      display: inline-block;
-      box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, .5),
-        7px 7px 20px 0px rgba(0, 0, 0, .1),
-        4px 4px 5px 0px rgba(0, 0, 0, .1);
-      outline: none;
-    }
-    .btn-1 {
-      background: rgb(6, 14, 131);
-      background: linear-gradient(0deg, rgba(6, 14, 131, 1) 0%, rgba(12, 25, 180, 1) 100%);
-      border: none;
-    }
+.intro_text {
+  text-align: right;
+}
 
-    .btn-1:hover {
-      background: rgb(0, 3, 255);
-      background: linear-gradient(0deg, rgba(0, 3, 255, 1) 0%, rgba(2, 126, 251, 1) 100%);
-    }
+.btn-1 {
+  left: 45%;
+  margin-top: 50px;
+  align-self: center;
+  text-align: center;
+  width: 130px;
+  height: 40px;
+  color: #fff;
+  border-radius: 5px;
+  padding: 10px 25px;
+  font-family: 'Lato', sans-serif;
+  font-weight: 500;
+  /* background: transparent; */
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+  box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, .5),
+    7px 7px 20px 0px rgba(0, 0, 0, .1),
+    4px 4px 5px 0px rgba(0, 0, 0, .1);
+  outline: none;
+}
+
+.btn-1 {
+  background: rgb(6, 14, 131);
+  background: linear-gradient(0deg, rgba(6, 14, 131, 1) 0%, rgba(12, 25, 180, 1) 100%);
+  border: none;
+}
+
+.btn-1:hover {
+  background: rgb(0, 3, 255);
+  background: linear-gradient(0deg, rgba(0, 3, 255, 1) 0%, rgba(2, 126, 251, 1) 100%);
+}
 </style>
--- client/src/views/RegisterPage.vue
+++ client/src/views/RegisterPage.vue
@@ -14,8 +14,12 @@
             <el-input placeholder="请输入密码" prefix-icon="el-icon-lock" v-model="password" show-password>
             </el-input>
           </el-form-item>
-          <el-form-item prop="password">
+          <el-form-item prop="password_confirm">
             <el-input placeholder="请再次输入密码" prefix-icon="el-icon-lock" v-model="password_confirm" show-password>
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="email">
+            <el-input placeholder="请输入邮箱" prefix-icon="el-icon-office-building" v-model="email">
             </el-input>
           </el-form-item>
           <el-button type="primary" class="loginBtn" @click="send_data()">注册</el-button>
@@ -31,14 +35,10 @@ import axios from 'axios'
 export default {
   data () {
     return {
-      // ruleForm: {
-      //   username: '',
-      //   password: '',
-      //   password_confirm: ''
-      // }
       username: '',
       password: '',
-      password_confirm: ''
+      password_confirm: '',
+      email: ''
     }
   },
   mounted () {
@@ -51,48 +51,181 @@ export default {
     back_login () {
       window.location.href = '/#/login'
     },
+    is_upper (tmpChar) {
+      var asciiNum = tmpChar.charCodeAt(0)
+      if ((asciiNum >= 65 && asciiNum <= 90)) {
+        return true
+      } else {
+        return false
+      }
+    },
+    is_lower (tmpChar) {
+      var asciiNum = tmpChar.charCodeAt(0)
+      if ((asciiNum >= 97 && asciiNum <= 122)) {
+        return true
+      } else {
+        return false
+      }
+    },
+    is_alpha (tmpChar) {
+      var asciiNum = tmpChar.charCodeAt(0)
+      if ((asciiNum >= 65 && asciiNum <= 90) || (asciiNum >= 97 && asciiNum <= 122)) {
+        return true
+      } else {
+        return false
+      }
+    },
+    is_digit (tmpChar) {
+      var asciiNum = tmpChar.charCodeAt(0)
+      if ((asciiNum >= 48 && asciiNum <= 57)) {
+        return true
+      } else {
+        return false
+      }
+    },
+    is_special (tmpChar) {
+      var specialSymbol = ['!', ',', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+', '{', '}', '[', ']', ':', ';', ',', '.', '/', '?', '<', '>']
+      for (var i = 0; i < specialSymbol.length; i++) {
+        if (tmpChar === specialSymbol[i]) {
+          return true
+        }
+      }
+      return false
+    },
+    is_username_valid (username) {
+      if (username.length < 8 || username.length > 20) {
+        return false
+      }
+      var hasDigit = 0
+      var hasAlpha = 0
+      var hasSpecialSymbol = 0
+      for (var i = 0; i < username.length; i++) {
+        if (hasDigit + hasAlpha + hasSpecialSymbol === 3) {
+          break
+        }
+        var tmpChar = username[i]
+        if (hasDigit === 0) {
+          if (this.is_digit(tmpChar) === true) {
+            hasDigit = 1
+          }
+        }
+        if (hasAlpha === 0) {
+          if (this.is_alpha(tmpChar) === true) {
+            hasAlpha = 1
+          }
+        }
+        if (hasSpecialSymbol === 0) {
+          if (this.is_special(tmpChar) === true) {
+            hasSpecialSymbol = 1
+          }
+        }
+      }
+      if (hasDigit + hasAlpha + hasSpecialSymbol !== 3) {
+        return false
+      }
+      return true
+    },
+    is_password_valid (password) {
+      if (password.length < 8 || password.length > 20) {
+        return false
+      }
+      var hasDigit = 0
+      var hasUpperAlpha = 0
+      var hasLowerAlpha = 0
+      var hasSpecialSymbol = 0
+      for (var i = 0; i < password.length; i++) {
+        if (hasDigit + hasUpperAlpha + hasLowerAlpha + hasSpecialSymbol === 4) {
+          break
+        }
+        var tmpChar = password[i]
+        if (hasDigit === 0) {
+          if (this.is_digit(tmpChar) === true) {
+            hasDigit = 1
+          }
+        }
+        if (hasUpperAlpha === 0) {
+          if (this.is_upper(tmpChar) === true) {
+            hasUpperAlpha = 1
+          }
+        }
+        if (hasLowerAlpha === 0) {
+          if (this.is_lower(tmpChar) === true) {
+            hasLowerAlpha = 1
+          }
+        }
+        if (hasSpecialSymbol === 0) {
+          if (this.is_special(tmpChar) === true) {
+            hasSpecialSymbol = 1
+          }
+        }
+      }
+      if (hasDigit + hasUpperAlpha + hasLowerAlpha + hasSpecialSymbol !== 4) {
+        return false
+      }
+      return true
+    },
     send_data: function () {
       var dataobj = {
         'username': this.username,
         'password': this.password,
-        'email': 'test.mail.com'
+        'email': this.email
       }
-      window.alert(dataobj['username'])
+      if (this.is_username_valid(dataobj['username']) === false) {
+        this.$message.error('用户名不合法！')
+        return
+      }
+      if (this.is_password_valid(dataobj['password']) === false) {
+        this.$message.error('密码格式不对！')
+        return
+      }
+      if (this.password !== this.password_confirm) {
+        this.$message.error('两次输入的密码不匹配！')
+        return
+      }
+      const path = 'http://127.0.0.1:8000/register/'
+      var configGet = {
+        method: 'POST',
+        url: path,
+        headers: {
+          'User-Agent': 'Apifox/1.0.0 (https://www.apifox.cn)',
+          'Content-Type': 'application/json'
+        },
+        data: dataobj
+      }
+      axios(configGet)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data))
+          if (response.status === 200) {
+            window.alert('注册成功')
+            window.location.href = '/#/MainPage'
+          }
+        })
+        .catch(function (error) {
+          console.log(error)
+          if (error.response.status === 401) {
+            window.alert('重复的用户名！')
+          }
+        })
+      var config = {
+        method: 'POST',
+        url: path,
+        headers: {
+          'User-Agent': 'Apifox/1.0.0 (https://www.apifox.cn)',
+          'Content-Type': 'application/json'
+        },
+        data: dataobj
+      }
       // arrived here
-      axios.post('http://localhost:8080/register/', dataobj)
-      // axios({
-      //   method: 'post',
-      //   url: 'http://localhost:8080/register/',
-      //   headers: {
-      //     'User-Agent': 'Apifox/1.0.0 (https://www.apifox.cn)',
-      //     'Content-Type': 'application/json'
-      //   },
-      //   data: dataobj
-      // })
-      //   .then(function (response) {
-      //     console.log(JSON.stringify(response.data))
-      //   })
-      //   .catch(function (error) {
-      //     console.log(error)
-      //   })
-      // }
+      axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data))
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
     }
   }
 }
-// var data = {
-//   'username': this.username,
-//   'password': this.password,
-//   'email': 'test.mail.com'
-// }
-// var config = {
-//   methods: 'post',
-//   url: 'http://127.0.0.1:8000/register',
-//   headers: {
-//     'User-Agent': 'Apifox/1.0.0 (https://www.apifox.cn)',
-//     'Content-Type': 'application/json'
-//   },
-//   data: data
-// }
 </script>
 
 <style>
