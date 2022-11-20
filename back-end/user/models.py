286ad8b81fd805d54c1868984aa7103ed8b60329
liclyde <gdrqdsh@outlook.com>
2022-11-20 14:14:55 +0800

    models完善

--- back-end/user/models.py
+++ back-end/user/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 
 class UserInfo(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True,verbose_name='注册日期')
     id = models.AutoField(verbose_name='id', primary_key= True)
     username = models.CharField(verbose_name='用户名', max_length=32)
     password = models.CharField(verbose_name='密码', max_length=64)
     email = models.CharField(verbose_name='Email', max_length = 64)
+    
+    class Meta:
+        ording = ('create_date')
+
+
 class PatientInfo(models.Model):
     name = models.CharField(verbose_name='姓名', max_length=16)
     age = models.IntegerField(verbose_name='年龄')
@@ -16,3 +22,44 @@ class PatientInfo(models.Model):
         (2, '女'),
     )
     gender = models.SmallIntegerField(verbose_name='性别', choices=gender_choices)
+    disease_types = (
+        (1,'一型糖尿病'),
+        (2,'二型糖尿病')
+    )
+    disease_type = models.SmallIntegerField(verbose_name='糖尿病类型', choices=disease_types)
+    userinfo = models.OneToOneField(UserInfo,on_delete=models.CASCADE)
+
+class datum(models.Model):
+    blood_glucose = models.FloatField(verbose_name='血糖值',null=True)
+    weight = models.FloatField(verbose_name='体重',null=True)
+    blood_pressure = models.FloatField(verbose_name='血压',null=True)
+    ketone = models.FloatField(verbose_name='血酮',null=True)
+    time_tags = (
+        (1, '空腹'),
+        (2, '早餐后'),
+        (3, '午餐前'),
+        (4, '午餐后'),
+        (5, '晚餐前'),
+        (6, '晚餐后'),
+    )
+    time_tag = models.SmallIntegerField(verbose_name='录入时段', choices=time_tags)
+    notes = models.CharField(verbose_name='备注')
+    date = models.DateField(verbose_name='日期',auto_now_add=True)
+    user = models.ForeignKey(UserInfo,related_name='user_date')
+
+
+class sports_record(models.Model):
+    date = models.DateField(verbose_name='日期',auto_now_add=True)
+    sport_type = models.CharField(verbose_name='运动类型', null=True)
+    notes = models.CharField(verbose_name='备注',null=True)
+    user = models.ForeignKey(UserInfo,related_name='user_date')
+
+
+
+
+    
+    
+
+
+
+
