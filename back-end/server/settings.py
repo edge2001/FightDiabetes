662fc40ed18a3b947b7633546b6f86c150c633ae
ChimpOnCloud <sunfy20@mails.tsinghua.edu.cn>
2022-12-21 17:24:08 +0800

    sports page modifying

--- back-end/server/settings.py
+++ back-end/server/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'user',
         'USER': 'root',
-        'PASSWORD': 'chachaw',
+        'PASSWORD': 'sfy20020708',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
